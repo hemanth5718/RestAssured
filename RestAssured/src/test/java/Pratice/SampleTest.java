087e3230d3c8aad4a37505c9d493017533cb6efa
hemanth5718 <hemanthgowda571@gmail.com>
2025-09-15 15:15:28 +0530

    h

---
+++ RestAssured/src/test/java/Pratice/SampleTest.java
@@ -0,0 +1,18 @@
+package Pratice;
+
+public class SampleTest {
+
+	public static void given() {
+		System.out.println("given");
+	}
+
+	public static void when() {
+		System.out.println("when");
+
+	}
+
+	public static void then() {
+		System.out.println("then");
+
+	}
+}
---
+++ RestAssured/src/test/java/backendTest/run.java
@@ -0,0 +1,18 @@
+package backendTest;
+
+import static Pratice.SampleTest.given;
+import static Pratice.SampleTest.then;
+import static Pratice.SampleTest.when;
+
+import org.testng.annotations.Test;
+
+
+public class run {
+	@Test
+	public void testing() {
+		given();
+		when();
+		then();
+	}
+
+}
